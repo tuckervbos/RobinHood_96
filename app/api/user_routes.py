3f0f5b59f23e52538713ca2415529fd8187a055c
graysonslater <grays@Based_Pc.localdomain>
2024-12-20 17:44:25 -0500

    finished testing user routes

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -53,34 +53,38 @@ def update_user(user_id):
         'user': user.to_dict() 
         })
 
-@user_routes.route('/user/<int:user_id>', methods=['DELETE'])
+@user_routes.route('/<int:user_id>', methods=['DELETE'])
 @login_required 
 def delete_user(user_id): 
-    user = users.pop(user_id, None) 
+    user = User.query.filter_by(id=user_id).first()
+     
     if not user: 
         return jsonify({'message': 'User not found'}), 404 
+    db.session.delete(user)
+    db.session.commit()
     return jsonify({'message': 'User deleted successfully'})
 
-@user_routes.route('/user/<int:user_id>/add_money', methods=['POST'])
+@user_routes.route('/<int:user_id>/add_money', methods=['POST'])
 @login_required 
 def add_money(user_id): 
-    user = users.get(user_id) 
+    user = User.query.filter_by(id=user_id).first()
+    print()
     if not user: 
         return jsonify({'message': 'User not found'}), 404 
     data = request.get_json() 
     amount = data.get('amount', 0) 
-    user.balance += amount 
-    return jsonify({'message': 'Money added successfully', 'balance': user.balance})
+    user.account_balance += amount 
+    return jsonify({'message': 'Money added successfully', 'account_balance': user.account_balance})
 
-@user_routes.route('/user/<int:user_id>/withdraw_money', methods=['POST'])
+@user_routes.route('/<int:user_id>/withdraw_money', methods=['POST'])
 @login_required 
 def withdraw_money(user_id): 
-    user = users.get(user_id) 
+    user = User.query.filter_by(id=user_id).first()
     if not user: 
         return jsonify({'message': 'User not found'}), 404 
     data = request.get_json() 
     amount = data.get('amount', 0) 
-    if amount > user.balance: 
-        return jsonify({'message': 'Insufficient balance'}), 400 
-    user.balance -= amount 
-    return jsonify({'message': 'Money withdrawn successfully', 'balance': user.balance})
+    if amount > user.account_balance: 
+        return jsonify({'message': 'Insufficient account_balance'}), 400 
+    user.account_balance -= amount 
+    return jsonify({'message': 'Money withdrawn successfully', 'balance': user.account_balance})
