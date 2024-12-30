7e2933089414926089a9ec4849b2b4a1a4cbabf1
Thao88bee <beethao.bt@gmail.com>
2024-12-30 16:14:43 -0600

    LoginSignup

--- react-vite/src/components/LoginFormPage/LoginFormPage.jsx
+++ react-vite/src/components/LoginFormPage/LoginFormPage.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import { thunkLogin } from "../../redux/session";
+import { useEffect, useState } from "react";
+import { demoLogin, thunkLogin } from "../../redux/session";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate, useNavigate, Link } from "react-router-dom";
 import "./LoginForm.css";
 
 function LoginFormPage() {
@@ -11,6 +11,15 @@ function LoginFormPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    if (email.length <= 4 || password.length <= 6) {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  }, [email, password]);
 
   if (sessionUser) return <Navigate to="/" replace={true} />;
 
@@ -31,34 +40,123 @@ function LoginFormPage() {
     }
   };
 
+  const demo = async (e) => {
+    e.preventDefault();
+    await dispatch(demoLogin());
+  };
+
   return (
     <>
-      <h1>Log In</h1>
-      {errors.length > 0 &&
-        errors.map((message) => <p key={message}>{message}</p>)}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.password && <p>{errors.password}</p>}
-        <button type="submit">Log In</button>
-      </form>
+      <div className="outterLogin">
+        <div className="bubbleSec">
+          <span style={{ "--i": 21 }}>$</span>
+          <span style={{ "--i": 16 }}>$</span>
+          <span style={{ "--i": 17 }}>$</span>
+          <span style={{ "--i": 23 }}>$</span>
+          <span style={{ "--i": 28 }}>$</span>
+          <span style={{ "--i": 25 }}>$</span>
+          <span style={{ "--i": 18 }}>$</span>
+          <span style={{ "--i": 22 }}>$</span>
+          <span style={{ "--i": 19 }}>$</span>
+          <span style={{ "--i": 12 }}>$</span>
+          <span style={{ "--i": 15 }}>$</span>
+          <span style={{ "--i": 30 }}>$</span>
+          <span style={{ "--i": 10 }}>$</span>
+          <span style={{ "--i": 20 }}>$</span>
+          <span style={{ "--i": 21 }}>$</span>
+          <span style={{ "--i": 16 }}>$</span>
+          <span style={{ "--i": 17 }}>$</span>
+          <span style={{ "--i": 23 }}>$</span>
+          <span style={{ "--i": 28 }}>$</span>
+          <span style={{ "--i": 25 }}>$</span>
+          <span style={{ "--i": 18 }}>$</span>
+          <span style={{ "--i": 22 }}>$</span>
+          <span style={{ "--i": 19 }}>$</span>
+          <span style={{ "--i": 12 }}>$</span>
+          <span style={{ "--i": 15 }}>$</span>
+          <span style={{ "--i": 30 }}>$</span>
+          <span style={{ "--i": 20 }}>$</span>
+          <span style={{ "--i": 17 }}>$</span>
+          <span style={{ "--i": 21 }}>$</span>
+          <span style={{ "--i": 16 }}>$</span>
+          <span style={{ "--i": 17 }}>$</span>
+          <span style={{ "--i": 23 }}>$</span>
+          <span style={{ "--i": 28 }}>$</span>
+          <span style={{ "--i": 25 }}>$</span>
+          <span style={{ "--i": 18 }}>$</span>
+          <span style={{ "--i": 22 }}>$</span>
+          <span style={{ "--i": 19 }}>$</span>
+          <span style={{ "--i": 12 }}>$</span>
+          <span style={{ "--i": 15 }}>$</span>
+          <span style={{ "--i": 30 }}>$</span>
+          <span style={{ "--i": 20 }}>$</span>
+          <span style={{ "--i": 17 }}>$</span>
+          <span style={{ "--i": 23 }}>$</span>
+          <span style={{ "--i": 15 }}>$</span>
+          <span style={{ "--i": 18 }}>$</span>
+          <span style={{ "--i": 16 }}>$</span>
+          <span style={{ "--i": 13 }}>$</span>
+          <span style={{ "--i": 26 }}>$</span>
+          <span style={{ "--i": 22 }}>$</span>
+          <span style={{ "--i": 11 }}>$</span>
+          <span style={{ "--i": 15 }}>$</span>
+          <span style={{ "--i": 18 }}>$</span>
+          <span style={{ "--i": 31 }}>$</span>
+          <span style={{ "--i": 19 }}>$</span>
+          <span style={{ "--i": 22 }}>$</span>
+          <span style={{ "--i": 27 }}>$</span>
+          <span style={{ "--i": 25 }}>$</span>
+          <span style={{ "--i": 15 }}>$</span>
+          <span style={{ "--i": 17 }}>$</span>
+          <span style={{ "--i": 14 }}>$</span>
+        </div>
+        <div className="loginSec">
+          <h1>Log In</h1>
+          {errors.length > 0 &&
+            errors.map((message) => <p key={message}>{message}</p>)}
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>
+                <input
+                  className="loginInput"
+                  placeholder="Email"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </label>
+              {errors.email && <p>{errors.email}</p>}
+            </div>
+            <div>
+              <label>
+                <input
+                  className="loginInput"
+                  placeholder="Password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </label>
+              {errors.password && <p>{errors.password}</p>}
+            </div>
+            <button className="loginButton" type="submit" disabled={disabled}>
+              Log In
+            </button>
+            <br />
+            <button className="loginDemo" onClick={demo}>
+              Login in as Demo User
+            </button>
+            <p>
+              Don&apos;t have a account?{" "}
+              <Link className="loginLink" to={"/signup"}>
+                Signup Here
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
--- react-vite/src/redux/session.js
+++ react-vite/src/redux/session.js
@@ -40,6 +40,25 @@ export const thunkLogin = (credentials) => async dispatch => {
   }
 };
 
+export const demoLogin = () => async (dispatch) => {
+  const user = { email: "demo4@aa.io", password: "password" };
+  const response = await fetch("api/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(user),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(setUser(data));
+  } else if (response.status < 500) {
+    const errorMessages = await response.json();
+    return errorMessages;
+  } else {
+    return { server: "Something went wrong. Please try again" };
+  }
+};
+
 export const thunkSignup = (user) => async (dispatch) => {
   const response = await fetch("/api/auth/signup", {
     method: "POST",
