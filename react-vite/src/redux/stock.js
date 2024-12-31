dc26d60e91bed8b393e5caaeba0fd98cf69cc7fa
Xiaoxue <xiaoxuew895@gmail.com>
2024-12-31 13:47:57 -0800

    search bar

--- react-vite/src/redux/stock.js
+++ react-vite/src/redux/stock.js
@@ -2,6 +2,7 @@
 
 // action types
 const SHOW_ALL_STOCKS = 'stocks/SHOW_ALL_STOCKS';
+const SHOW_SEARCH_RESULTS = 'stocks/SHOW_SEARCH_RESULTS';
 const SHOW_ONE_STOCK = 'stock/SHOW_ONE_STOCK';
 
 // action creators
@@ -11,6 +12,13 @@ const showAllStocks = (stocks)=>{
         payload: stocks,
     }
 }
+
+const showSearchResults = (results) => {
+    return {
+      type: SHOW_SEARCH_RESULTS,
+      payload: results,
+    };
+  };
 
 const showOneStock = (stock)=>{
     return{
@@ -35,7 +43,8 @@ export const showAllStocksSearchThunk = (input)=>async(dispatch)=>{
     const res = await fetch(`/api/stocks/search?input=${input}`);
     if(res.ok){
         const data = await res.json()
-        dispatch(showAllStocks(data.results_list))
+        // console.log(data[0].results_list)
+        dispatch(showSearchResults(data[0].results_list))
     }else{
         const error = await res.json();
         throw error;    
@@ -57,13 +66,16 @@ export const showOneStockThunk = (stockId) =>async(dispatch)=>{
 //reducer
 const initialState = {
     currentStock: null,
-    stocks: []
+    stocks: [],
+    searchResults: [],
 }
 
 const stockReducer = (state = initialState,action)=>{
     switch(action.type){
         case SHOW_ALL_STOCKS:
             return{...state,stocks:action.payload}
+        case SHOW_SEARCH_RESULTS: 
+            return { ...state,searchResults: action.payload };
         case SHOW_ONE_STOCK:
             return{...state,currentStock:action.payload}
         default:
