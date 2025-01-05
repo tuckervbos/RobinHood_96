671eb0067a7dadb82697d1dd872088e8762796ea
graysonslater <grays@Based_Pc.localdomain>
2025-01-04 19:05:08 -0500

    debugging in progress

--- react-vite/src/components/UserProfile/UserProfile.jsx
+++ react-vite/src/components/UserProfile/UserProfile.jsx
@@ -1,11 +1,19 @@
 import { useDispatch, useSelector } from "react-redux";
+
 import { useEffect, useState } from "react";
+
 import { Link, useNavigate } from "react-router-dom";
-import { thunkDeleteUser, thunkLogout } from "../../redux/session";
+
+import { thunkDeleteUser, thunkLogout,editUser, getUserById, userNameCheck, emailCheck } from "../../redux/session";
+
 import { getAllPortfolios } from "../../redux/portfolios";
+
 import { showWatchlistsThunk } from "../../redux/watchlist";
+
 import StockTickerAnimation from "../StockTickerAnimation/StockTickerAnimation";
+
 import CustomModal from "../AllPortfolios/CustomModal";
+
 import "./UserProfile.css";
 
 const UserProfile = () => {
@@ -17,10 +25,14 @@ const UserProfile = () => {
   const user = useSelector((state) => state.session.user);
   const portfolios = useSelector((state) => state.portfolios.allPortfolios);
   const watchlists = useSelector((state) => state.watchlist.watchlists);
+  console.log("FRONT STATE USER=" ,user," PORTFOLIOS= ", portfolios, "WATCHLISTS= ", watchlists)
+  
+
 
   useEffect(() => {
     dispatch(showWatchlistsThunk());
     dispatch(getAllPortfolios());
+    dispatch(getUserById(user.id))
   }, [dispatch]);
 
   const logout = (e) => {
@@ -40,18 +52,102 @@ const UserProfile = () => {
     setShowDelete(!showDelete);
   };
 
+/***********************************************************************************************************************************************/
+//*                            Edit button Modal
+/***********************************************************************************************************************************************/
+  
+const [showEdit, setShowEdit] = useState(false);
+const [errors, setEditErrors] = useState({});
+// const [userToEdit, setUserToEdit] = useState(); //!!!!
+// const [editName,setEditName] = useState(); //!!!!
+const [username,setUsername] = useState(user.username);
+const [firstname, setFirstname] = useState(user.firstname);
+const [lastname, setLastname] = useState(user.lastname);
+const [email, setEmail] = useState(user.email);   
+console.log("FRONT EDIT STATE username= ", username, " FIRSTNAME= ", firstname, " lastname= ", lastname, " email= ", email, " ERRORS= ", errors)
+
+useEffect(() => {
+  if (user) {
+    setUsername(user.username);
+    setFirstname(user.firstname);
+    setLastname(user.lastname);
+    setEmail(user.email);
+  }
+}, [dispatch, user]);
+
+const handleEdit = async (e, username, firstname, lastname, email) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("fRONT TEST2= ", username, email)
+    setEditErrors({});
+    let validationErrors = {};
+    if (username.length < 1){
+        validationErrors.username = "UserName must be at least 1 character";
+    }
+    if (firstname.length < 1){
+      validationErrors.firstname = "first name must be at least 1 character";
+    }
+    if (lastname.length < 1){
+      validationErrors.lastname = "last name must be at least 1 character";
+    }
+    const userNameTaken = await dispatch(userNameCheck(username));
+    console.log("USER TAKEN =", userNameTaken.exists)
+    if (userNameTaken.exists && userNameTaken.exists.username != user.username){
+      validationErrors.username = `${username} is already taken!`;
+    }
+    const emailTaken = await dispatch(emailCheck(email));
+    
+    if (emailTaken.exists && emailTaken.exists.email != user.email){
+      validationErrors.email = `${email} is already taken!`;
+    }
+    if (Object.keys(validationErrors).length > 0) { //!ERROR HANDLING MUST BE ADDED IN HTML
+        setEditErrors(validationErrors);
+        return;
+    }
+    console.log("FRONT dispatch TEST= ", username, firstname, lastname, email )
+    dispatch(editUser({username, firstname, lastname, email,userId: user.id})); //!!!!
+    setShowEdit(false);
+};
+
+//toggle for modal
+const editEvent = (e) => { //!DOES THE EDIT EVENT NEED THE USER OBJ??? (I THINK NO)
+  e.preventDefault();
+  e.stopPropagation();
+  setUsername(user.username);
+  setFirstname(user.firstname);
+  setLastname(user.lastname);
+  setEmail(user.email);   
+  setShowEdit(!showEdit);
+};
+
+
+/***********************************************************************************************************************************************/
+//*                            Deposite funds button
+/***********************************************************************************************************************************************/
+  
+const [money, setMoney] = useState();
+
+const handleDeposite = (e, money)=> {
+  e.preventDefault();
+  e.stopPropagation();
+  dispatch(depositFunds({money,userId:user.id}))
+  dispatch(getUserById(user.id))
+  };
+
+
+/***********************************************************************************************************************************************/
   return (
     <>
       <div className="userProfile">
         <div className="top">
-          <h1>{user?.username} Profile</h1>
+          <h1>{user.username} Profile</h1>
           <button onClick={logout}>Log Out</button>
         </div>
         <div className="profileSec1">
           <div className="userImage">
             <div className="glowBack">
               <img
-                title={`${user?.username} profile picture`}
+                title={`${user.username} profile picture`}
                 src="https://i.pinimg.com/736x/a8/4a/a3/a84aa310f33862e53c30f55bdf94b013.jpg"
                 alt="{`${user.username} profile picture`}"
               />
@@ -62,13 +158,28 @@ const UserProfile = () => {
               {user?.firstname}, {user?.lastname}
             </h2>
             <p>${user?.account_balance}</p>
-            <button className="profileEditBtn">Edit Profile</button>
+            <button type="button" className="profileEditBtn" onClick={(e) => editEvent(e)}>Edit User</button>
             <button className="profileDeleteBtn" onClick={showModal}>
               Delete Profile
             </button>
           </div>
         </div>
       </div>
+      <>
+      {/* ADD FUNDS BUTTON */}
+        <h2 className="addFunds">Add Funds</h2>
+          <p className="userAccountBalance">Account balance: {user.account_balance}</p>
+          <label className="addFunds">                 
+              <input
+                  type="integer"
+                  placeholder="$"
+                  value={money}
+                  onChange={(e) => setMoney(e.target.value)}
+              />   
+          </label> 
+          <button type="button" onClick={(e) => handleDeposit(e, money)}>Deposit</button>
+          <button type="button" >Withdraw</button>
+      </>
       <div>
         <StockTickerAnimation />
       </div>
@@ -157,6 +268,53 @@ const UserProfile = () => {
           </CustomModal>
         )}
       </>
+
+      {/* Edit PORTFOLIO MODAL */}
+      <>{showEdit && 
+          <CustomModal onClose={(e) => editEvent(e)}>
+              <div className='editUserTitle'>Edit User info</div>
+              <div className='editButtons'>
+              <label className="editUsername">
+                  UserName:                  
+                  <input
+                      type="text"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                  />
+                   {errors.username && <span className="profileErrors">{errors.username}</span>}
+              </label>   
+              <label className="editFirstname">  
+                  First name:               
+                  <input
+                      type="text"
+                      value={firstname}
+                      onChange={(e) => setFirstname(e.target.value)}
+                  /> 
+                  {errors.firstname && <span className="profileErrors">{errors.firstname}</span>}
+              </label> 
+              <label className="editLastname">
+                  Last name:                 
+                  <input
+                      type="text"
+                      value={lastname}
+                      onChange={(e) => setLastname(e.target.value)}
+                  />
+                  {errors.lastname && <span className="profileErrors">{errors.lastname}</span>}  
+              </label> 
+              <label className="editEmail">  
+                  Email:               
+                  <input
+                      type="text"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                  /> 
+                  {errors.email && <span className="profileErrors">{errors.email}</span>}  
+              </label> 
+              <button type="button" onClick={(e) => handleEdit(e, username, firstname, lastname, email)}>Confirm Change</button>
+              <button type="button" onClick={editEvent}>cancel</button>
+              </div>
+          </CustomModal>}
+        </>
     </>
   );
 };
--- react-vite/src/redux/session.js
+++ react-vite/src/redux/session.js
@@ -1,20 +1,40 @@
-const SET_USER = 'session/setUser';
-const REMOVE_USER = 'session/removeUser';
-const DELETE_USER = 'session/deleteUser';
+/***********************************************************************************************************************************************/
+//*                             ACTION OBJECTS
+/***********************************************************************************************************************************************/
 
+const SET_USER = 'session/setUser';
 const setUser = (user) => ({
   type: SET_USER,
   payload: user
 });
 
+const REMOVE_USER = 'session/removeUsr';
 const removeUser = () => ({
   type: REMOVE_USER
 });
 
+const USERNAME_CHECK = 'session/userNamecheck';
+const userNameCheckAO = (user) => ({
+  type: USERNAME_CHECK,
+  payload: user
+});
+
+const EMAIL_CHECK = 'session/emailCheck';
+const emailCheckAO = (user) => ({
+  type: EMAIL_CHECK,
+  payload: user
+});
+
+const DELETE_USER = 'session/deleteUser';
 const deleteUser = () => ({
   type: DELETE_USER,
 })
 
+/***********************************************************************************************************************************************/
+//*                            THUNKS
+/***********************************************************************************************************************************************/
+
+//autherize
 export const thunkAuthenticate = () => async (dispatch) => {
 	const response = await fetch("/api/auth/");
 	if (response.ok) {
@@ -22,11 +42,11 @@ export const thunkAuthenticate = () => async (dispatch) => {
 		if (data.errors) {
 			return;
 		}
-
-		dispatch(setUser(data));
+	dispatch(setUser(data));
 	}
 };
 
+//login
 export const thunkLogin = (credentials) => async dispatch => {
   const response = await fetch("/api/auth/login", {
     method: "POST",
@@ -45,8 +65,9 @@ export const thunkLogin = (credentials) => async dispatch => {
   }
 };
 
+//demo login
 export const demoLogin = () => async (dispatch) => {
-  const user = { email: "demo1@aa.io", password: "password" };
+  const user = { email: "demo4@aa.io", password: "password" };
   const response = await fetch("api/auth/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -64,6 +85,7 @@ export const demoLogin = () => async (dispatch) => {
   }
 };
 
+//signup
 export const thunkSignup = (user) => async (dispatch) => {
   const response = await fetch("/api/auth/signup", {
     method: "POST",
@@ -82,11 +104,72 @@ export const thunkSignup = (user) => async (dispatch) => {
   }
 };
 
+//logout
 export const thunkLogout = () => async (dispatch) => {
   await fetch("/api/auth/logout");
   dispatch(removeUser());
 };
 
+//get one user, needed for use with a dispatch 
+export const getUserById = (userId) => async (dispatch) => {
+    const request = await fetch(`/api/portfolios/${userId}`,{
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+    const response = await request.json();
+    dispatch(setUser(response));
+    return response;
+};
+
+//update user
+export const editUser = (info) => async (dispatch) => {
+    const request = await fetch(`/api/users/${info.userId}`, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: info.username,
+          firstname: info.firstname,
+          lastname: info.lastname,
+          email: info.email
+        })
+    })
+    const response = await request.json();
+    dispatch(getUserById(info.userId));
+    return response;
+};
+
+//UserName check
+export const userNameCheck = (username) => async (dispatch) => {
+  console.log("STORE username= ", username)
+  const request = await fetch(`/api/users/userNameCheck/${username}`,{
+      method: "GET",
+      headers: {
+          'Content-Type': 'application/json',
+      }
+  });
+  const response = await request.json();
+  dispatch(userNameCheckAO(response));
+  return response;
+};
+
+//email check
+export const emailCheck = (email) => async (dispatch) => {
+  const request = await fetch(`/api/users/emailCheck/${email}`,{
+      method: "GET",
+      headers: {
+          'Content-Type': 'application/json',
+      }
+  });
+  const response = await request.json();
+  dispatch(emailCheckAO(response));
+  return response;
+};
+
+//delete user
 export const thunkDeleteUser = (userId) => async (dispatch) => {
   const response = await fetch(`/api/users/${userId}`, {
     method: "DELETE",
@@ -100,7 +183,23 @@ export const thunkDeleteUser = (userId) => async (dispatch) => {
   }
 }
 
-const initialState = { user: null };
+//deposite funds
+export const depositFunds = (info) => async (dispatch) => {
+  const request = await fetch(`/api/users/${info.userId}/add_money`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({money:info.money})
+  });
+  const response = await request.json();
+  dispatch(getUserById(info.userId));
+  return response;
+};
+
+/***********************************************************************************************************************************************/
+//*                             REDUCER
+/***********************************************************************************************************************************************/
+
+const initialState = { user: null, userNameCheckState: [], emailCheckState: [] };
 
 function sessionReducer(state = initialState, action) {
   switch (action.type) {
@@ -108,8 +207,12 @@ function sessionReducer(state = initialState, action) {
       return { ...state, user: action.payload };
     case REMOVE_USER:
       return { ...state, user: null };
+    case USERNAME_CHECK:
+      return {...state, userNameCheckState: action.payload};
+    case EMAIL_CHECK:
+      return {...state, emailCheckState: action.payload};
     case DELETE_USER:
-      return { ...state, user: action.payload }
+        return { ...state, user: action.payload }
     default:
       return state;
   }
